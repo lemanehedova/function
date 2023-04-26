fcee93f8c0b78ce689d39bb8b16e3030592b336f
lemanehedova <127186173+lemanehedova@users.noreply.github.com>
2023-04-25 21:12:12 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+// 1 eded daxil edin. Eger o eded 50den kicikdirse o edede qeder 3-e bolunen,
+//  50-100 araligindadirsa o edede qeder 5e bolunen, 100den boyukdurse o edede qeder 8 e bolunen 
+// ededlerin sayini ekrana cixardan bir alqoritm yazin.
+
+
+if(x>0 && x<50 ){
+    var x;
+    console.log("count= 16");
+}
+ else if( x>50 && x< 100){
+    var x;
+    console.log("count=9");
+}
+ else if (x >100){
+    var x;
+    console.log("count=112");
+}
+
+// Methodlar yaradirsiz. Methoda ad gonderende adi ekrana cixardir(Sabir),
+//  ad ve soyad gonderende soyadi adi ekrana cixardir(Guliyev Sabir),
+//   ad,soyad ve ata adı göndərildikdə adin baş hərfi nöqtə soyad ata adi ekrana cixardirsiz(S.Guliyev Mehti). 
+//   Methodlarin adi eyni olmalıdır.
+
+function person(name){
+        console.log("Salam "+name);
+    }
+    person("Sabir");
+    person("Guliyev");
+    person("S.Guliyev Mehti")
+    person(prompt())
+
+
+// Toplama çıxma, toplama, vurma, bölməyə uyğun funksiya yaradirsiz 2 eded ve 1 operator('+','-','*','/') qebul edir.
+//  Qebul etdiyi operatora uygun a və b ədədləri üzərində hesablama aparib alinan deyeri qaytarır.
+ if(find cem )  {
+    function sum(x,y){
+        console.log(Number(x)+Number(y));
+    }
+    
+    sum(prompt(), prompt());
+}
+  If (find hasil) {
+    
+    function mul(x,y){
+        console.log(Number(x)*Number(y));
+    }
+}
+    
+    mul(prompt(), prompt());
+
+
+    If (find nisbet) {
+    
+        function dis(x,y){
+            console.log(Number(x)/Number(y));
+        }
+    
+        
+        mul(prompt(), prompt());
+    }  
+        
+        
+        If (find ferq) {
+    
+            function diff(x,y){
+                console.log(Number(x)-Number(y));
+            }
+        }
+            
+            mul(prompt(), prompt());
+        
+
+
+
+    
